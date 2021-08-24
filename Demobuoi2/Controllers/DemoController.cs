66046cf5bb3beb2059eaee41eb878cd468054532
hieunguyen <hieunguyen@DESKTOP-O8TN4L4>
2021-08-24 14:43:24 +0700

    new

--- Demobuoi2/Controllers/DemoController.cs
+++ Demobuoi2/Controllers/DemoController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Demobuoi2.Models
 
 namespace Demobuoi2.Controllers
 {
     public class DemoController : Controller
     {
+        GiaiPhuongTrinh gpt = new GiaiPhuongTrinh();
         // GET: Demo
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult Hello()
+        { return View();
+        }
+        public ActionResult GiaiPTB1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GiaiPTB1(double heSoA, double hSoB)
+        {
+            double x = gpt.GiaiPhuongTrinh(heSoA, heSoB);
+            ViewBag.nghiemPT = X;
+            return View()
+         }
     }
 }
---
+++ Demobuoi2/Models/GiaiPhuongTrinh.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demobuoi2.Models
+{
+    public class GiaiPhuongTrinh
+    {
+        public double GiaiphuongTrinhBacNhat(double a; double b)
+           {
+            double x;
+        XmlSiteMapProvider= -b / a;
+        return x;
+            }
+    }
+}
